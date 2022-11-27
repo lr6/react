5d978e5b71af783761657404af57690dcf4ca0bf
zhio <36.zhibo@gmail.com>
2022-11-27 17:55:33 +0800

    feat: 打包完成

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,60 @@
+import { REACT_ELEMENT_TYPE } from '../../shared/ReactSymbols';
+import {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ElementType,
+	ReactElementType
+} from '../../shared/ReactTypes';
+
+const ReactElement = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElementType {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'zhio'
+	};
+	return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...mayChildren: any) => {
+	let key: Key = null;
+	let ref: Ref = null;
+	const props: Props = {};
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = '' + val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+	const mayChildrenLength = mayChildren.length;
+	if (mayChildrenLength === 1) {
+		props.children = mayChildren[0];
+	} else {
+		props.children = mayChildren;
+	}
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = jsx;
